d18edf844406b4cec1fdade2e8726e0337134551
rubensandstrom@gmail.com <rubensandstrom@gmail.com>
2021-11-30 14:07:37 +0100

    get input as Vec<Vec<&str>>, will make input Vec<Vec<i32>>

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("../input").expect("Wrong input!");
+    let input: Vec<&str> = input.split('\n')
+                                .collect();
+    let mut format_input: Vec<Vec<i32>> = Vec::new();
+    for i in input {
+        format_input.push(i.split('x')
+                    .parse()
+                    .unwrap_or(0)
+                    .collect());
+    }
+    println!("{:?}", &format_input);
+
 }
