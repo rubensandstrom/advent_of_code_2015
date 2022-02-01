9a335d8077ba2a7c8819a6280b53e33c4041ab79
rubensandstrom@gmail.com <rubensandstrom@gmail.com>
2022-02-01 19:39:41 +0100

    removed day 2

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2015/day2/target/debug/day2: /Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2015/day2/src/main.rs
--- day2/target/debug/deps/day2-70bae5396903e442.d
+++
@@ -1,5 +0,0 @@
-/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2015/day2/target/debug/deps/day2-70bae5396903e442: src/main.rs
-
-/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2015/day2/target/debug/deps/day2-70bae5396903e442.d: src/main.rs
-
-src/main.rs:
