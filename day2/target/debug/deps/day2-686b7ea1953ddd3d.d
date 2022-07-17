1973efa5dc278a28d0a6813460242a609cd5f4a1
rubensandstrom@gmail.com <rubensandstrom@gmail.com>
2022-07-17 14:02:51 +0200

    initial commit

---
+++ day2/target/debug/deps/day2-686b7ea1953ddd3d.d
@@ -0,0 +1,5 @@
+/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2015/day2/target/debug/deps/day2-686b7ea1953ddd3d: src/main.rs
+
+/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2015/day2/target/debug/deps/day2-686b7ea1953ddd3d.d: src/main.rs
+
+src/main.rs:
---
+++ day2/target/debug/deps/day2-9106c1c706f568eb.d
@@ -0,0 +1,5 @@
+/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2015/day2/target/debug/deps/day2-9106c1c706f568eb: src/main.rs
+
+/Users/rubensandstrom/Documents/Programmering/Rust/advent_of_code_2015/day2/target/debug/deps/day2-9106c1c706f568eb.d: src/main.rs
+
+src/main.rs:
